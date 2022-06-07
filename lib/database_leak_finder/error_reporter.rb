61a48d10167bd50defcf8fc8d9e329e2f73a3cd6
Josh Justice <jjustice@justworks.com>
2022-06-07 15:41:58 -0400

    Improve error message

--- lib/database_leak_finder/error_reporter.rb
+++ lib/database_leak_finder/error_reporter.rb
@@ -1,9 +1,19 @@
-require 'highline'
-
 module DatabaseLeakFinder
   class ErrorReporter
-    def report(example, leaks)
-      raise "THERE WAS AN ERROR"
+    def log(example, leaks)
+      message_rows =
+        ["The spec '#{spec_path_for(example)}' leaves the following rows in the database:"]
+        + leaks.each(&method(:format_leak))
+      raise message_rows.join(' ')
+    end
+
+    private
+    def spec_path_for(example)
+      example.metadata[:example_group][:file_path]
+    end
+
+    def format_leak(table, count)
+      "#{count} #{'row'.pluralize(count)} for the #{table} table"
     end
   end
 end
